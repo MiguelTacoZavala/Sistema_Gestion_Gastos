124a3cff87c8f5b5bf8226a62f5eefc61da0464e
MiguelTacoZavala <zangelomt@gmail.com>
2026-03-19 03:01:42 -0500

    Correcion del Login page, estructura y refactoriazacion de archivos css

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -6,36 +6,34 @@ import Button from "../UI/Button";
 export default function Login() {
   return (
     <>
-      <div className="left-view">
-        <header>
+      <section className="form-section">
+        <header className="form-header">
           <img src={bankIcon} alt="Bank Icon" className="bank-icon" />
-          <h1>Guardian de gastos</h1>
+          <h1 className="form-header__title">Guardian de gastos</h1>
         </header>
-        <div className="form-title">
-          <h1>Inicia sesion</h1>
-          <form action="submit" className="login-form">
-            <Input
-              id="username"
-              label="Usuario"
-              type="text"
-              placeholder="Usuario"
-            />
-            <Input
-              id="password"
-              label="Contraseña"
-              type="password"
-              placeholder="Contraseña"
-            />
-            <Button text="Iniciar sesion" type="login"></Button>
-          </form>
-        </div>
-      </div>
-      <div className="right-view">
+        <h1 className="form-section__title">Inicia sesion</h1>
+        <form action="submit" className="login-form">
+          <Input
+            id="username"
+            label="Usuario"
+            type="text"
+            placeholder="Usuario"
+          />
+          <Input
+            id="password"
+            label="Contraseña"
+            type="password"
+            placeholder="Contraseña"
+          />
+          <Button text="Iniciar sesion" type="login"></Button>
+        </form>
+      </section>
+      <section className="welcome-section">
         <div className="content-app">
-            <h1>Bienvenido</h1>
-            <p>Esto es una pagina diseñada con el propósito de <span>ayudarte a gestionar tus gastos de manera eficiente</span></p>
+          <h1>Bienvenido</h1>
+          <p>Esto es una pagina diseñada con el propósito de <span>ayudarte a gestionar tus gastos de manera eficiente</span></p>
         </div>
-      </div>
+      </section>
     </>
   );
 }
