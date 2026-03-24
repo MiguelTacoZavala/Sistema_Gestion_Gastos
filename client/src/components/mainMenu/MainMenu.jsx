b621dcfa1c011ca0d0969fb537e41a8050e729af
LeonardoVera <leonardovera20037@gmail.com>
2026-03-24 07:05:20 -0500

    peticion POST del cliente para añadir nuevo gasto

--- client/src/components/mainMenu/MainMenu.jsx
+++ client/src/components/mainMenu/MainMenu.jsx
@@ -2,8 +2,8 @@ import Sidebar from "./SideBar"
 import "./MainMenu.css"
 import Input from "../UI/Input"
 import Button from "../UI/Button"
-import {useState} from "react"
-import {useEffect} from "react"
+import { useState } from "react"
+import { useEffect } from "react"
 
 export default function MainMenu() {
 
@@ -16,6 +16,33 @@ export default function MainMenu() {
         }
     }, []);
 
+    const handleAddGasto = (e) => {
+        e.preventDefault();
+
+        fetch(import.meta.env.VITE_LOCAL_HOST + "/addGasto", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                userId: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")).id : null,
+                descripcion: "descripcion",
+                monto: 100,
+                fecha: "2024-06-01",
+                categoria: "Alimentacion"
+            }),
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Gasto agregado:", data);
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            })
+    }
+
     return (
         <main className="main-menu">
             <Sidebar />
@@ -25,10 +52,10 @@ export default function MainMenu() {
                     <Input id="descripcion" label="Descripcion" type="text"></Input>
                     <Input id="monto" label="Monto" type="decimal"></Input>
                     <Input id="fecha" label="Fecha" type="date"></Input>
-                    <Input id="tipo" label="Tipo" type="select" options={categorias}></Input>
+                    <Input id="categoria" label="Categoria" type="select" options={categorias}></Input>
                 </div>
                 <div className="main-menu-form__btn">
-                    <Button text="Agregar gasto" type="add-gasto"></Button>
+                    <Button text="Agregar gasto" type="add-gasto" onClick={handleAddGasto}></Button>
                 </div>
             </form>
         </main>
