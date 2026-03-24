db61670c03a8374e8c07096aa4f3b0c3f0f5e158
MiguelTacoZavala <zangelomt@gmail.com>
2026-03-24 01:16:36 -0500

    cambios y correciones

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,11 +1,13 @@
+import { generateToken } from "../services/tokenService";
+
 const router = require("express").Router();
-const user = require("../models/User");
+const User = require("../models/User");
 
 // Ruta de inicio de sesión
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const [user, created] = await user.findOrCreate({
+    const [user, created] = await User.findOrCreate({
       where: { username },
       defaults: { password },
     });
@@ -15,12 +17,15 @@ router.post("/login", async (req, res) => {
       return res.status(401).json({ message: "Contraseña incorrecta" });
     }
 
+    // Generamos el token JWT para el usuario autenticado
+    const token = generateToken(user);
     const message = created
       ? "Usuario creado y autenticado"
       : "Usuario autenticado";
     return res.json({
       message,
       user: { id: user.id, username: user.username },
+      tokenGenerado: token,
     });
   } catch (error) {
     console.error("Error en la autenticación:", error);
